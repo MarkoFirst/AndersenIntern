bcb10de436a96496166d42f30cedca766ad363d9
MarkoFirst <babiichuck.vadim@yandex.ru>
2018-03-20 11:28:36 +0200

    исправил баг отправки сообщений

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -39,9 +39,6 @@ export class UsersComponent implements OnInit {
   }
 
   checkChat(chat: string): void {
-
-    console.log('тут', this.currentUser);
-
     if (this.currentUser.chats[chat] !== undefined) {
       this.enterInRealChat(this.currentUser.chats[chat]);
     } else {
